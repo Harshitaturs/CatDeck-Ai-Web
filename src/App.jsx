6571f6fab1b77b0e214bdcf11fead861188fdbc2
unknown <h0651302@gmail.com>
2026-03-28 21:59:15 +0530

    Fixed 404 with Redirect to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import ChatPage from "./pages/chat/ChatPage";
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
