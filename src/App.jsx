bf349f62641c75f31476f1b5318c5c80cd58e3ce
unknown <h0651302@gmail.com>
2026-03-28 22:07:13 +0530

    Fix: Cleaned App.jsx routes and added vercel rewrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import ChatPage from "./pages/chat/ChatPage";
-import NotFoundPage from "./pages/notFound/NotFoundPage";
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
